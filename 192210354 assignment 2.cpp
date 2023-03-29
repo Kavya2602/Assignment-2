ed3a70574bffacefc5a6d2a0f630ebf467b498f9
Kavya2602 <129164816+Kavya2602@users.noreply.github.com>
2023-03-29 21:33:02 +0530

    Add files via upload

---
+++ 192210354 assignment 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int a[5] = {12, 56, 34, 78, 100};
+
+printf("%d\n",a[12]);
+printf("%d\n",a[56]);
+printf("%d\n",a[34]);
+printf("%d\n",a[78]);
+printf("%d\n",a[100]);
+
+return 0;
+
+}
